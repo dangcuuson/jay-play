4cf4878b2682299dcf65aabeae91cfb39a08447b
jack <dangcuuson@gmail.com>
2017-04-27 23:07:55 +1000

    add chess images

--- src/components/ChessPiece.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-class ChessPiece extends React.Component {
-    
-}
---
+++ src/logic/ChessConstants.js
@@ -0,0 +1,9 @@
+export const CHESS_PIECE_PAWN = "PAWN";
+export const CHESS_PIECE_KNIGHT = "KNIGHT";
+export const CHESS_PIECE_ROOK = "ROOK";
+export const CHESS_PIECE_BISHOP = "BISHOP";
+export const CHESS_PIECE_QUEEN = "QUEEN";
+export const CHESS_PIECE_KING = "KING";
+
+export const CHESS_SIDE_WHITE = "WHITE";
+export const CHESS_SIDE_BLACK = "BLACK";
